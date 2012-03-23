ae3b32d2a3da58cf449e4c97b36c6c4dddad47dd
Kara Yu <lele.yu@gmail.com>
2012-03-23 01:37:00 -0400

    Initial Commit

---
+++ project2Tests/project2Tests.h
@@ -0,0 +1,13 @@
+//
+//  project2Tests.h
+//  project2Tests
+//
+//  Created by Kara Yu on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface project2Tests : SenTestCase
+
+@end
